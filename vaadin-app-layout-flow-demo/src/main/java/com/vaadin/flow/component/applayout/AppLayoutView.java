44fa1799fc60a44e55c00c6c87fcf47583899216
Tulio Garcia <tulio@vaadin.com>
2019-03-20 15:56:35 +0200

    Fix demo

--- vaadin-app-layout-flow-demo/src/main/java/com/vaadin/flow/component/applayout/AppLayoutView.java
+++ vaadin-app-layout-flow-demo/src/main/java/com/vaadin/flow/component/applayout/AppLayoutView.java
@@ -13,6 +13,7 @@ import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.demo.DemoView;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteConfiguration;
 
 @Route("vaadin-app-layout")
 public class AppLayoutView extends DemoView {
@@ -22,9 +23,14 @@ public class AppLayoutView extends DemoView {
         addCard("App Layout usage in a demo application",
                 new Div(new Label("Try out the demo which is using the `vaadin-app-layout-flow` component. "),
                         new Anchor("https://bakery-flow.demo.vaadin.com/login", "Open demo.")));
-        appLayoutWithBrandingLogo();
-        appLayoutWithBrandingText();
-        appLayoutWithMenus();
+
+        // Don't actually run, because it would require valid routes for links
+        //appLayoutWithBrandingLogo();
+        //appLayoutWithBrandingText();
+        //appLayoutWithMenus();
+        addCard("Basic App Layout with text branding");
+        addCard("Simple App Layout with brand logo");
+        addCard("App Layout with Action Menu Item");
     }
 
     private void appLayoutWithBrandingText() {
@@ -51,10 +57,10 @@ public class AppLayoutView extends DemoView {
             img.setHeight("44px");
             appLayout.setBranding(img);
 
-            menu.addMenuItems(new AppLayoutMenuItem("Page 1", "page1"),
-                    new AppLayoutMenuItem("Page 2", "page2"),
-                    new AppLayoutMenuItem("Page 3", "page3"),
-                    new AppLayoutMenuItem("Page 4", "page4"));
+            menu.addMenuItems(new AppLayoutMenuItem("Page 1", Page1.class),
+                    new AppLayoutMenuItem("Page 2", Page2.class),
+                    new AppLayoutMenuItem("Page 3", Page3.class),
+                    new AppLayoutMenuItem("Page 4", Page4.class));
 
             Component content = new Span(new H3("Page title"),
                     new Span("Page content"));
@@ -77,8 +83,8 @@ public class AppLayoutView extends DemoView {
             AppLayoutMenu menu = appLayout.createMenu();
 
             menu.addMenuItems(
-                    new AppLayoutMenuItem(VaadinIcon.USER.create(), "My Profile", "profile"),
-                    new AppLayoutMenuItem(VaadinIcon.TRENDING_UP.create(), "Trending Topics", "trends"),
+                    new AppLayoutMenuItem(VaadinIcon.USER.create(), "My Profile", Profile.class),
+                    new AppLayoutMenuItem(VaadinIcon.TRENDING_UP.create(), "Trending Topics", Trends.class),
                     new AppLayoutMenuItem(VaadinIcon.SIGN_OUT.create(), "Sign Out", e -> logout()));
             // end-source-example
             // @formatter:on
@@ -92,4 +98,10 @@ public class AppLayoutView extends DemoView {
     private void logout() {
     }
 
+    static class Page1 extends Div{};
+    static class Page2 extends Div{};
+    static class Page3 extends Div{};
+    static class Page4 extends Div{};
+    static class Profile extends Div{};
+    static class Trends extends Div{};
 }
