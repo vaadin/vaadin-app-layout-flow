954ebfad4dc0898d17da8141cb44d6912a788b38
Tulio Garcia <tulio@vaadin.com>
2019-03-22 17:18:23 +0200

    Added new properties

--- vaadin-app-layout-flow/src/main/java/com/vaadin/flow/component/applayout/AppLayout.java
+++ vaadin-app-layout-flow/src/main/java/com/vaadin/flow/component/applayout/AppLayout.java
@@ -9,9 +9,9 @@ package com.vaadin.flow.component.applayout;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,6 +24,8 @@ import java.util.Objects;
 
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.HasElement;
+import com.vaadin.flow.component.PropertyDescriptor;
+import com.vaadin.flow.component.PropertyDescriptors;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.dom.Element;
@@ -36,8 +38,49 @@ import com.vaadin.flow.router.RouterLayout;
 @Tag("vaadin-app-layout")
 @HtmlImport("frontend://bower_components/vaadin-app-layout/src/vaadin-app-layout.html")
 public class AppLayout extends Component implements RouterLayout {
+    private static final PropertyDescriptor<String, String> orientationProperty = PropertyDescriptors
+        .propertyWithDefault("orientation",
+            Orientation.VERTICAL.toPropertyValue());
+    private static final PropertyDescriptor<Boolean, Boolean> drawerFirstProperty = PropertyDescriptors
+        .propertyWithDefault("drawerFirst", false);
+    private static final PropertyDescriptor<Boolean, Boolean> drawerOpenedProperty = PropertyDescriptors
+        .propertyWithDefault("drawerOpened", false);
+    private static final PropertyDescriptor<Boolean, Boolean> overlayProperty = PropertyDescriptors
+        .propertyWithDefault("overlay", false);
 
     private Component content;
+
+    public Orientation getOrientation() {
+        return Orientation.fromPropertyValue(orientationProperty.get(this));
+    }
+
+    public void setOrientation(Orientation orientation) {
+        orientationProperty.set(this, orientation.toPropertyValue());
+    }
+
+    public boolean isDrawerFirst() {
+        return drawerFirstProperty.get(this);
+    }
+
+    public void setDrawerFirst(boolean drawerFirst) {
+        drawerFirstProperty.set(this, drawerFirst);
+    }
+
+    public boolean isDrawerOpened() {
+        return drawerOpenedProperty.get(this);
+    }
+
+    public void setDrawerOpened(boolean drawerOpened) {
+        drawerOpenedProperty.set(this, drawerOpened);
+    }
+
+    public boolean isOverlay() {
+        return overlayProperty.get(this);
+    }
+
+    public void setOverlay(boolean overlay) {
+        overlayProperty.set(this, overlay);
+    }
 
     /**
      * Returns the {@link Element}
@@ -90,7 +133,7 @@ public class AppLayout extends Component implements RouterLayout {
      * This hook is called before a navigation is being made into a route
      * which has this router layout as its parent layout.
      *
-     * @param content  {@link HasElement} the content component being added
+     * @param content {@link HasElement} the content component being added
      */
     protected void beforeNavigate(Component content) {
     }
@@ -99,9 +142,24 @@ public class AppLayout extends Component implements RouterLayout {
      * This hook is called after a navigation is made into a route
      * which has this router layout as its parent layout.
      *
-     * @param content  {@link HasElement} the content component added
+     * @param content {@link HasElement} the content component added
      */
     protected void afterNavigate(Component content) {
     }
 
+    public enum Orientation {
+        VERTICAL, HORIZONTAL;
+
+        private String toPropertyValue() {
+            return this.name().toLowerCase();
+        }
+
+        private static Orientation fromPropertyValue(String property) {
+            if (property == null) {
+                return null;
+            } else {
+                return Orientation.valueOf(property.toUpperCase());
+            }
+        }
+    }
 }
