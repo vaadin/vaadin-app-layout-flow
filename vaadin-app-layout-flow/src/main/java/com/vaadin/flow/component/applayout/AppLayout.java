25418cc3e940a66e9e12029d20669b58642851e2
Tulio Garcia <tulio@vaadin.com>
2019-03-25 17:41:20 +0200

    Added drawer.

--- vaadin-app-layout-flow/src/main/java/com/vaadin/flow/component/applayout/AppLayout.java
+++ vaadin-app-layout-flow/src/main/java/com/vaadin/flow/component/applayout/AppLayout.java
@@ -9,9 +9,9 @@ package com.vaadin.flow.component.applayout;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,8 +20,6 @@ package com.vaadin.flow.component.applayout;
  * #L%
  */
 
-import java.util.Objects;
-
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.HasComponents;
 import com.vaadin.flow.component.HasElement;
@@ -29,8 +27,9 @@ import com.vaadin.flow.component.PropertyDescriptor;
 import com.vaadin.flow.component.PropertyDescriptors;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.HtmlImport;
-import com.vaadin.flow.dom.Element;
 import com.vaadin.flow.router.RouterLayout;
+
+import java.util.Objects;
 
 /**
  * Server-side component for the {@code <vaadin-app-layout>} element.
@@ -46,7 +45,7 @@ public class AppLayout extends Component implements RouterLayout,
     private static final PropertyDescriptor<Boolean, Boolean> drawerFirstProperty = PropertyDescriptors
         .propertyWithDefault("drawerFirst", false);
     private static final PropertyDescriptor<Boolean, Boolean> drawerOpenedProperty = PropertyDescriptors
-        .propertyWithDefault("drawerOpened", false);
+        .propertyWithDefault("drawerOpened", true);
     private static final PropertyDescriptor<Boolean, Boolean> overlayProperty = PropertyDescriptors
         .propertyWithDefault("overlay", false);
 
