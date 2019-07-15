83b75f7b1b00732ab67c126e4216e87d741727d9
Guillermo Alvarez <guillermo@vaadin.com>
2019-07-15 18:43:14 +0300

    Add advanced demo with routerlink

--- vaadin-app-layout-flow-demo/src/main/java/com/vaadin/flow/component/applayout/AppLayoutView.java
+++ vaadin-app-layout-flow-demo/src/main/java/com/vaadin/flow/component/applayout/AppLayoutView.java
@@ -1,18 +1,22 @@
 package com.vaadin.flow.component.applayout;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.html.Span;
-import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.page.Viewport;
 import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.demo.DemoView;
+import com.vaadin.flow.router.AfterNavigationEvent;
+import com.vaadin.flow.router.AfterNavigationObserver;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouterLink;
 import com.vaadin.flow.server.PWA;
 
 @Route("vaadin-app-layout")
@@ -20,26 +24,26 @@ public class AppLayoutView extends DemoView {
 
     @Override
     protected void initView() {
-        addCard("App Layout usage in a demo application",
-                new Div(new Label("Try out the demo which is using the `vaadin-app-layout-flow` component. "),
-                        new Anchor("https://bakery-flow.demo.vaadin.com/login", "Open demo.")));
-        addCard("Basic App Layout");
-        addCard("App layout with drawer as primary section");
+        addCard("App Layout usage in a demo application", new Div(new Label(
+                "Try out the demo which is using the `vaadin-app-layout-flow` component. "),
+                new Anchor("https://bakery-flow.demo.vaadin.com/login",
+                        "Open demo.")));
+        addCard("App layout with nabvar menu");
+        addCard("App layout with drawer menu");
         addCard("App layout in mobile");
         addCard("App layout as main view for PWA");
+        addCard("Routing example", "App layout with RouterLink");
     }
 
     // @formatter:off
     // begin-source-example
-    // source-example-heading: Basic App Layout
-    public class BasicAppLayoutView extends AppLayout {
-        public BasicAppLayoutView() {
+    // source-example-heading: App layout with nabvar menu
+    public class AppLayoutWithNavbarMenu extends AppLayout {
+        public AppLayoutWithNavbarMenu() {
             Image img = new Image("https://i.imgur.com/GPpnszs.png", "Vaadin Logo");
-            img.setHeight("44px");
-            addToNavbar(new DrawerToggle(), img);
             Tabs tabs = new Tabs(new Tab("Home"), new Tab("About"));
-            tabs.setOrientation(Tabs.Orientation.VERTICAL);
-            addToDrawer(tabs);
+            img.setHeight("44px");
+            addToNavbar(img, tabs);
         }
     }
     // end-source-example
@@ -47,9 +51,9 @@ public class AppLayoutView extends DemoView {
 
     // @formatter:off
     // begin-source-example
-    // source-example-heading: App layout with drawer as primary section
-    public class AppLayoutWithDrawerView extends AppLayout {
-        public AppLayoutWithDrawerView() {
+    // source-example-heading: App layout with drawer menu
+    public class AppLayoutWithDrawerMenu extends AppLayout {
+        public AppLayoutWithDrawerMenu() {
             setPrimarySection(AppLayout.Section.DRAWER);
             Image img = new Image("https://i.imgur.com/GPpnszs.png", "Vaadin Logo");
             img.setHeight("44px");
@@ -76,7 +80,7 @@ public class AppLayoutView extends DemoView {
             addToDrawer(tabs);
         }
     }
-     // end-source-example
+    // end-source-example
     // @formatter:on
 
     // @formatter:off
@@ -93,6 +97,62 @@ public class AppLayoutView extends DemoView {
             Tabs tabs = new Tabs(new Tab("Home"), new Tab("About"));
             tabs.setOrientation(Tabs.Orientation.VERTICAL);
             addToDrawer(tabs);
+        }
+    }
+    // end-source-example
+    // @formatter:on
+
+    // @formatter:off
+    // begin-source-example
+    // source-example-heading: App layout with RouterLink
+    public class MainView extends AppLayout implements AfterNavigationObserver {
+
+        private Tabs tabs = new Tabs();
+        // List will contain all the hrefs in the same order as they were added to tabs
+        // ["", "admin", "dashboard"]
+        // index is used to setSelectedIndex on afterNavigation
+        private List<String> hrefs = new ArrayList<>();
+
+        public MainView() {
+            addMenuTab("Main", DefaultView.class);
+            addMenuTab("Admin", AdminView.class);
+            addMenuTab("Dashboard", DashboardView.class);
+            tabs.setOrientation(Tabs.Orientation.VERTICAL);
+            addToDrawer(tabs);
+            addToNavbar(new DrawerToggle());
+        }
+
+        private void addMenuTab(String text, Class<? extends Component> navigationTarget) {
+            //Each tab will contain a RouterLink that navigates to the navigationTarget
+            RouterLink link = new RouterLink(text, navigationTarget);
+            hrefs.add(link.getHref());
+            tabs.add(new Tab(link));
+        }
+
+        @Override
+        public void afterNavigation(AfterNavigationEvent event) {
+            tabs.setSelectedIndex(hrefs.indexOf(event.getLocation().getFirstSegment()));
+        }
+    }
+
+    @Route(value = "", layout = MainView.class)
+    public class DefaultView extends Div {
+        public DefaultView() {
+            add(new Span("Default view content"));
+        }
+    }
+
+    @Route(value = "admin", layout = MainView.class)
+    public class AdminView extends Div {
+        public AdminView() {
+            add(new Span("Admin view content"));
+        }
+    }
+
+    @Route(value = "dashboard", layout = MainView.class)
+    public class DashboardView extends Div {
+        public DashboardView() {
+            add(new Span("Dashboard view content"));
         }
     }
     // end-source-example
